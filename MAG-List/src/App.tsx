56fceea77cdab33f9b706ac4a3bcc31d4fe1cb3d
Tiago Borges <tiagoc.borges02@gmail.com>
2024-09-09 12:39:23 +0100

    miniUpdates

--- MAG-List/src/App.tsx
+++ MAG-List/src/App.tsx
@@ -1,15 +1,15 @@
-import {Routes, Route} from 'react-router-dom';
-import "./App.css";
-import Home from "./pages/Home";
-function App() {
+// src/App.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css'; 
+import Home from './pages/Home'; 
 
+const App: React.FC = () => {
   return (
-    <>
-     <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
